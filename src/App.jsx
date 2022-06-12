0dcfe9791946679ea12ed913a2dadd0b70ef3f69
mamun-63 <mamun.cse63@gmail.com>
2022-06-12 12:39:01 +0600

    csv file read and extract

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+
+ function App() {
+   const [file, setFile] = useState();
+   const [array, setArray] = useState([]);
+ 
+   const fileReader = new FileReader();
+ 
+   const handleOnChange = (e) => {
+     console.log('file: ', e.target.files);
+     setFile(e.target.files[0]);
+   };
+ 
+   const csvFileToArray = string => {
+     // not correctly extracting 
+     const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
+     const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
+ 
+     const array = csvRows.map(i => {
+       const values = i.split(",");
+       const obj = csvHeader.reduce((object, header, index) => {
+         object[header] = values[index];
+         return object;
+       }, {});
+       return obj;
+     });
+ 
+     setArray(array);
+   };
+ 
+   const handleOnSubmit = (e) => {
+     e.preventDefault();
+ 
+     if (file) {
+       fileReader.onload = function (event) {
+         console.log('onLoad event', event);
+         console.log(event.target.result);
+         const text = event.target.result;
+         csvFileToArray(text);
+       };
+ 
+       fileReader.readAsText(file);
+     }
+   };
+ 
+   const headerKeys = Object.keys(Object.assign({}, ...array));
+ 
+   return (
+     <div style={{ textAlign: "center" }}>
+       <h1>REACTJS CSV IMPORT EXAMPLE </h1>
+       <form>
+         <input
+           type={"file"}
+           id={"csvFileInput"}
+           accept={".csv"}
+           onChange={handleOnChange}
+         />
+ 
+         <button
+           onClick={(e) => {
+             handleOnSubmit(e);
+           }}
+         >
+           IMPORT CSV
+         </button>
+       </form>
+ 
+       <br />
+ 
+       <table>
+         <thead>
+           <tr key={"header"}>
+             {headerKeys.map((key) => (
+               <th>{key}</th>
+             ))}
+           </tr>
+         </thead>
+ 
+         <tbody>
+           {array.map((item) => (
+             <tr key={item.id}>
+               {Object.values(item).map((val) => (
+                 <td>{val}</td>
+               ))}
+             </tr>
+           ))}
+         </tbody>
+       </table>
+     </div>
+   );
+ }
+
+ export default App;
